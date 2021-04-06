777e2482da632f3ce0edd95f1201eeb36fa3aa4e
Burton85 <burton00tw@gmail.com>
2021-04-06 10:59:12 -0400

    Vue3 + Tailwind project init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
+    },
+    purge: ['@/**/*.html', '@/**/*.vue'],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+};
