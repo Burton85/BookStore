e7a0385119bec9184ef54c320ed3d7278674203e
Burton85 <burton00tw@gmail.com>
2021-04-07 14:52:58 -0400

    submit logic fix

--- src/composition/useDetail.ts
+++ src/composition/useDetail.ts
@@ -4,10 +4,12 @@ import { useRoute } from 'vue-router'
 
 export default function() {
     const route = useRoute()
-
+    
+    const rawBookDetail=ref({})
     const bookDetail=ref({price:0,count:0})
     const getBookDetail=()=>{
         dataGetter.getData('https://fe-interview-api.unnotech.com/profile/'+route.params.bookId).then(value=>{//獲取book detail資料            
+            rawBookDetail.value={price:value.price,count:value.count}//將原始數據淺拷貝出來
             bookDetail.value=value
         })
     }
@@ -30,16 +32,27 @@ export default function() {
     const id = computed(() => {//不能直接監聽route 會導致洩漏
         return route.params.bookId
      })
-
+    function isCountEqual(object1:any, object2:any) {
+        console.log(object1,object2)
+      return object1.count === object2.count;
+    }
+    function isPriceEqual(object1:any, object2:any) {
+        return object1.price === object2.price;
+    }
     const submitData=()=>{
-        dataPatcher.updateData("https://fe-interview-api.unnotech.com/profile/"+id.value,{price:bookDetail.value.price,count:bookDetail.value.count})
-        .then(res=>{
-            if(res.status==200){
-                alert('修改成功')
-            }else{
-                alert('網路異常')
-            }
-        })
+        if(isCountEqual(rawBookDetail.value,bookDetail.value)&&isPriceEqual(rawBookDetail.value,bookDetail.value)){
+            alert('請修改後再提交')
+        }else{
+            console.log(1)
+            dataPatcher.updateData("https://fe-interview-api.unnotech.com/profile/"+id.value,{price:bookDetail.value.price,count:bookDetail.value.count})
+            .then(res=>{
+                if(res.status==200){
+                    alert('修改成功')
+                }else{
+                    alert('網路異常')
+                }
+            })
+        }
     }
     watch(id, ()=>{
         getBookDetail()
