c3bc7f48612d1f9bf188e145d94a803e410efd04
Burton <32318847+Burton85@users.noreply.github.com>
2021-04-07 18:14:56 +0800

    fetch booklist

--- src/composition/List.ts
+++ src/composition/List.ts
@@ -1,12 +1,45 @@
-import { ref, onMounted } from 'vue';
+import { ref, onMounted,watch } from 'vue';
 import {dataGetter} from '../plugin/getdata'
 export default function() {
-    const list = ref([]);
-
+    const rawList = ref([]);
+    const filterList = ref([]);
+    const curIndex=ref(0);
+    const listStatus=ref('leftmost')
+    const nextBook = () => {
+        if(curIndex.value>rawList.value.length-5){
+            listStatus.value='rightmost'
+            return
+        }else{
+            curIndex.value++
+            if(curIndex.value>rawList.value.length-5){
+                listStatus.value='rightmost'
+            }
+        }
+    };
+    const pervBook = () => {
+        if(curIndex.value<=0){
+            listStatus.value='leftmost'
+            return
+        }else{
+            curIndex.value--
+            if(curIndex.value<=0){
+                listStatus.value='leftmost'
+            }
+        }
+    };
     dataGetter.getData('https://fe-interview-api.unnotech.com/books').then(value=>{
-        list.value=value
+            rawList.value=value;
+            filterList.value=value.slice(0,4)
     })
+    watch(curIndex, ()=>{
+        filterList.value=rawList.value.slice(curIndex.value,curIndex.value+4)
+    })
+
     return {
-        list,
+        curIndex,
+        nextBook,
+        pervBook,
+        filterList,
+        listStatus
     };
 }
