aa60b0c1e6eaabe2f026ab4e74f7a40434697c86
Burton <32318847+Burton85@users.noreply.github.com>
2021-04-07 18:13:03 +0800

    nest router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,32 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import Nest from '../views/Nest.vue'
 import Detail from '../components/Detail.vue'
 
 
 const routes: Array<RouteRecordRaw> = [
-  {path:'/',redirect:'books'},
   {
-    path: '/books',
-    name: 'Home',
-    component: Home,
+    path:'/:pathMatch(.*)*',
+    redirect:'/books'
   },
   {
-    path: '/books/:bookId',
-    name: 'Detail',
-    component: Detail,
-  }
+    path: '/books',
+    name: 'Books',
+    component: Nest,
+    children:[
+      {
+        path: '',
+        name: 'Home',
+        component: Home,
+      },
+      {
+        path: ':bookId',
+        name: 'Detail',
+        component: Detail,
+        props:true
+      }
+    ]
+  },
 ]
 
 const router = createRouter({
