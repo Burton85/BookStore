4c70ed7ae0aed18c4a1992f0da02e43e443dc2d2
Burton <32318847+Burton85@users.noreply.github.com>
2021-04-07 18:14:09 +0800

    nest router

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,5 @@
 <template>
     <div class="home">
-        <book-list />
+      home
     </div>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-import BookList from '../components/BookList.vue';
-
-export default defineComponent({
-    name: 'Home',
-    components: {
-        BookList,
-    },
-});
-</script>
