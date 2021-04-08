07d9ae17423d221c341fb47a84d0df4634913769
Burton <32318847+Burton85@users.noreply.github.com>
2021-04-08 11:25:31 +0800

    catch error

--- src/plugin/dataController.ts
+++ src/plugin/dataController.ts
@@ -8,22 +8,13 @@ const dataGetter={
                 return response.data;
             })
             .catch((error:any) => {
-                alert(error);
+                console.log(error);
             });
         return value;
     },
 }
 const dataPatcher={
     updateData : async (url:string,myData:any) => {
-        // let value = await axios
-        //     .patch(url,{'Content-Type': 'application/json' ,   responseType: 'json',data:myData})
-            // .then((response:any) => {
-            //     console.log(response)
-            //     return response;
-            // })
-            // .catch((error:any) => {
-            //     alert(error);
-            // });
         let value= await axios({
             method: 'patch',
             url: url,
@@ -35,7 +26,7 @@ const dataPatcher={
             }).then((response:any) => {
                 return response;
             }).catch((error:any) => {
-                alert(error);
+                console.log(error);
             });
         return value;
     },
